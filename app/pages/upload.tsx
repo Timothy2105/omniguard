dbc073032c7701c3b09a85b9eab07acfa1cfbe6a
timothy2105 <timothy12105@gmail.com>
2025-03-22 10:51:04 -0700

    feat: added upload video option

--- app/pages/upload.tsx
+++ app/pages/upload.tsx
@@ -1,8 +1,13 @@
+import UploadButton from '@/components/website/upload-button';
+
 export default function Upload() {
   return (
     <main className="flex flex-col items-center justify-center bg-black p-4">
       <div className="w-full max-w-md space-y-8 text-center">
-        <h1 className="text-3xl md:text-4xl font-bold text-white mb-6">Upload Video Page</h1>
+        <h1 className="text-3xl md:text-4xl font-bold text-white mb-6">Upload a video to analyze!</h1>
+        <div className="flex justify-center">
+          <UploadButton />
+        </div>
       </div>
     </main>
   );
