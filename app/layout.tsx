eddcc7045c57e790a5e530c7956acf36c82cfd81
timothy2105 <timothy12105@gmail.com>
2025-03-22 20:56:46 -0700

    fix: corrected children to span whole screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
                 </div>
               </nav>
 
-              <div>{children}</div>
+              <div className="w-full">{children}</div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
